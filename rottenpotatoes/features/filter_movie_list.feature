24109c4b5e63d1eb210d88b5793a2935bcc5b525
uzzsen <uzzsen@gmail.com>
2017-07-04 19:29:55 +0000

    movie list filter and sort features modified

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -21,12 +21,22 @@ Background: movies have been added to database
 
   And  I am on the RottenPotatoes home page
 
-Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
-
 Scenario: all ratings selected
   # see assignment
+When I check the following ratings: G, R, PG-13, PG, NC-17
+And I press "Refresh"
+Then I should see all the movies 
+
+Scenario: restrict to movies with 'PG' or 'R' ratings
+  
+# enter step(s) to check the "PG" and "R" checkboxes
+When I check the following ratings: PG, R 
+# enter step(s) to uncheck all other checkboxes
+And I uncheck the following ratings: PG-13, G, NC-17
+# enter step to "submit" the search form on the homepage
+And I press "Refresh"
+  # enter step(s) to ensure that PG and R movies are visible
+Then I should see movies with ratings: PG, R
+  # enter step(s) to ensure that other movies are not visible
+And I should not see movies with ratings: PG-13, G
+
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(title: movie["title"], rating: movie["rating"], release_date: movie["release_date"])
   end
-  fail "Unimplemented"
+  #fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +15,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  page.body.index(e1).should < page.body.index(e2) 
+  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +27,54 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+
+
+#  rating_list.scan(/[A-Z]*-?\d*/).delete_if {|i| i.empty?}.each do |rating|
+  rating_list.split(/, /).each do |rating|
+    #debugger
+  uncheck ? uncheck("ratings_" + rating) : check("ratings_" + rating)
+  end
+  #fail "Unimplemented"
+end
+
+Then /I should (not )?see movies with ratings: (.*)/ do |should_not, rating|
+	rating_list =[]	
+	rating.split(', ').each {|rting| rating_list << rting}
+		
+    if should_not
+    rating_list.each do |rting|	
+	page.find('#movies tbody').should have_no_content(/^#{rting}$/)
+	end    
+    else
+
+        rating_list.each do |rting|	
+        page.find('#movies tbody').should have_content(rting)
+	end 
+    end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.body.scan(/<tr>/).length #page.all('#movies tbody tr').count
+
+  #movie_titles = []
+  #Movie.all.each {|movie| movie_titles << movie.title}
+  
+  expect(rows).to eq Movie.all.length+1 #movie_titles.uniq.count
+  
+    #   page.all('table#movies tr').count.should == (Movie.count + 1)
+#	if $rows.respond_to? :should
+#  rows.count.should == Movie.count
+#	else
+#  assert rows.count == Movie.count
+#	end
+
+#  content = page.find_by_id('movies').text
+          #all_titles = movie_titles.all? do |title|
+ # debugger   
+     #content.include? title
+          #page.has_content? title
+	        #end
+#debugger
+         #expect(all_titles).to be true
 end
